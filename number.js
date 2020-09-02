90f62b1fba922db9f157011e503d387f40da1086
saiful2612 <bristykodom@gmail.com>
2020-09-02 07:12:30 +0600

    projects running

---
+++ number.js
@@ -0,0 +1,26 @@
+var number=Math.random();
+var pin=parseFloat(number);
+var Ceil=Math.ceil(pin);
+
+console.log(pin,Ceil);
+
+// console.log(pin.length);
+
+// var int_number = 2;
+// var int_length = (''+int_number).length;
+
+// if(int_length==1){
+//     var int_Number='000'+int_number;
+    
+// }
+// else if(int_length==2){
+//     var int_Number='00'+int_number;
+// }
+// else if(int_length==3){
+//     var int_Number='0'+int_number;
+// }
+// else{
+//     var int_Number=int_number;
+// }
+
+// console.log(int_Number);
